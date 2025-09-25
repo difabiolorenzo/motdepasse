8568ebe84c1a84ce9047e7d8ed064cdb8eb337dc
difabiolorenzo <difabiolorenzo@live.fr>
2025-09-25 20:29:18 +0200

    0.2.0

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 function init() {
     defaultVariables();
-
-    if (global.debug === true) {
-        initPassword();
-    }
+    initPassword();
 }
 
 function defaultVariables() {
     game = {
-        password: {
-            words: [],
-            index: 0,
-            passes_left: -1,
-        }
+        words: [],
+        index: 0,
+        passes_left: -1
     };
     global = {
-        version: [0, 1, 0],
+        version: [0, 2, 0],
         debug: false,
         modal_recap: new bootstrap.Modal(document.getElementById('modal_recap')),
-        modal_settings: new bootstrap.Modal(document.getElementById('modal_settings'))
+        modal_settings: new bootstrap.Modal(document.getElementById('modal_settings')),
+        globalTimerInterval: null,
+        wordTimerInterval: null,
+        automatic_timed_word_change_delay: 1000
     },
     settings = {
             word_amount: 5,
-            use_timer_total: false,
-            timer_total: 60,
+            use_timer_global: false,
+            timer_global: 30,
             use_timer_word: false,
-            timer_word: 60,
+            timer_word: 5,
+            automatic_timed_word_change: false,
             passes: -1,
-            show_hints: false
-    }
+            show_hints: false,
+            style: "2016"
+    };
+
+    document.getElementById("version").innerHTML = global.version;
     
     //Nombre de mot
-    document.getElementById("slider_word_amount").value = settings.word_amount
-    document.getElementById("slider_word_amount_value").innerHTML = settings.word_amount   
+    document.getElementById("slider_word_amount").value = settings.word_amount;
+    document.getElementById("slider_word_amount_value").innerHTML = settings.word_amount;
+
     //Activer le chronomètre global
-    document.getElementById("input_timer_global").checked = settings.use_timer_total
+    document.getElementById("input_timer_global").checked = settings.use_timer_global;
+
     //Durée totale
-    document.getElementById("slider_timer_total").value = settings.timer_total
-    document.getElementById("slider_timer_total_value").innerHTML = settings.timer_total
+    document.getElementById("slider_timer_global").value = settings.timer_global;
+    document.getElementById("slider_timer_global_value").innerHTML = settings.timer_global;
+
     //Activer le chronomètre par mot
-    document.getElementById("input_timer_word").checked = settings.use_timer_word 
+    document.getElementById("input_timer_word").checked = settings.use_timer_word;
+
     //Durée par mot
-    document.getElementById("slider_timer_word").value = settings.timer_word
-    document.getElementById("slider_timer_word_value").innerHTML = settings.timer_word 
+    document.getElementById("slider_timer_word").value = settings.timer_word;
+    document.getElementById("slider_timer_word_value").innerHTML = settings.timer_word;
+    document.getElementById("checkbox_timer_word_automatic_change").innerHTML = settings.automatic_timed_word_change;
+
     //Nombre de passes
-    document.getElementById("slider_max_passes").value = settings.passes
-    document.getElementById("slider_max_passes_value").innerHTML = settings.passes 
+    document.getElementById("slider_max_passes").value = settings.passes;
+    document.getElementById("slider_max_passes_value").innerHTML = settings.passes;
+
     //Activer les indices
-    document.getElementById("input_show_hints").checked = settings.show_hints 
+    document.getElementById("input_show_hints").checked = settings.show_hints;
 }
 
 const button_password_invalidate = document.getElementById("button_password_invalidate");
 const button_password_pass = document.getElementById("button_password_pass");
 const button_password_validate = document.getElementById("button_password_validate");
-
-function displayPage(id) {
-    const pages = ["menu", "game"];
-
-    for (const page of pages) {
-        document.getElementById(page).classList.add("d-none");
-    }
-    document.getElementById(id).classList.remove("d-none");
-}
 
 async function fetchWordsfromAPI() {
     const PASSWORD_WEBSITE_API = 'https://trouve-mot.fr/api/random/';
@@ -87,24 +87,23 @@ async function fetchWordsfromAPI() {
 }
 
 async function initializeWords() {
-    game.password.index = 0;
-    game.password.words = await fetchWordsfromAPI();
+    game.index = 0;
+    game.words = await fetchWordsfromAPI();
 
-    if (game.password.words.length === 0) {
+    if (game.words.length === 0) {
         alert("Aucun mot trouvé");
         return;
     }
 
     password_ingame_status.innerHTML = "";
-    for (const word of game.password.words) {
+    for (const word of game.words) {
         const span = document.createElement("span");
         span.classList.add("word_indicator");
         span.dataset.id = word.id; // 🔑 lien DOM ↔ mot
         password_ingame_status.appendChild(span);
     }
 
-    console.log("game.password.words", game.password.words);
-    console.log("game.password.index", game.password.index);
+    startPassword()
 }
 
 function initPassword() {
@@ -112,76 +111,77 @@ function initPassword() {
         alert(global.current_language_strings.password_internet_requierement);
         return;
     }
-
-    game.password.index = 0;
-    game.password.words = [];
-
-    button_password_invalidate.disabled = true;
-    button_password_pass.disabled = true;
-    button_password_validate.disabled = true;
-
-    password_ingame_display.innerHTML = "...";
-
-    document.getElementById("button_password_restart").classList.add("d-none");
-    document.getElementById("button_password_quit").classList.add("d-none");
-    document.getElementById("button_password_settings").classList.add("d-none");
-    document.getElementById("button_password_recap").classList.add("d-none");
-
-    document.getElementById("button_password_start").classList.remove("started");
-    document.getElementById("button_password_start").classList.add("not-started");
-
-    initializeWords();
-    displayPage("game");
+    game.index = 0;
+    game.words = [];
 }
 
 function startPassword() {
-    //Action enabled
-    document.getElementById("button_password_invalidate").disabled = false;
-    document.getElementById("button_password_pass").disabled = false;
-    document.getElementById("button_password_validate").disabled = false;
+    document.getElementById("rules").classList.add("d-none");
 
+    document.getElementById("title").classList.add("d-none");
+    document.getElementById("credits").classList.add("d-none");
+    document.getElementById("button_password_settings").classList.add("d-none");
+    document.getElementById("button_password_start").classList.add("d-none");
+    document.getElementById("button_password_recap").classList.add("d-none");
+    document.getElementById("exit_button").classList.remove("d-none");
+
+    document.getElementById("password_controls").classList.remove("not-started");
+    document.getElementById("password_controls").classList.remove("not-started");
+    document.getElementById("password_controls").classList.add("started");
     document.getElementById("password_ingame_content").classList.remove("not-started");
     document.getElementById("password_ingame_content").classList.add("started");
 
-    document.getElementById("button_password_start").classList.remove("not-started");
-    document.getElementById("button_password_start").classList.add("started");
-
-    document.getElementById("password_controls").classList.remove("not-started");
-    document.getElementById("password_controls").classList.add("started");
-
-    if (settings.use_timer_total == true) {
-    document.getElementById("password_timer_global").classList.remove("d-none");
-    }
+    //Action enabled
+    disableActionButtons(false);
 
     passwordDisplayNextWord();
+
+    if (settings.use_timer_global == true) {
+        document.getElementById("password_timer").classList.remove("d-none");
+        initTimerGlobal();
+    }
+
+    if (settings.use_timer_word == true) {
+        document.getElementById("password_timer").classList.remove("d-none");
+        initTimerWord();
+    }
 }
 
 function endPassword() {
-    //Action enabled
-    document.getElementById("button_password_invalidate").disabled = true;
-    document.getElementById("button_password_pass").disabled = true;
-    document.getElementById("button_password_validate").disabled = true;
+    document.getElementById("rules").classList.remove("d-none");
+
+    document.getElementById("title").classList.remove("d-none");
+    document.getElementById("credits").classList.remove("d-none");
+    document.getElementById("button_password_start").classList.remove("d-none");
+    document.getElementById("password_timer").classList.add("d-none");
+    document.getElementById("button_password_settings").classList.remove("d-none");
+    document.getElementById("button_password_recap").classList.remove("d-none");
+    document.getElementById("exit_button").classList.add("d-none");
 
     document.getElementById("password_ingame_content").classList.remove("started");
     document.getElementById("password_ingame_content").classList.add("not-started");
-
     document.getElementById("password_controls").classList.remove("started");
     document.getElementById("password_controls").classList.add("not-started");
 
-    document.getElementById("password_timer_global").classList.add("d-none");
-
-    document.getElementById("button_password_restart").classList.remove("d-none");
-    document.getElementById("button_password_quit").classList.remove("d-none");
-    document.getElementById("button_password_settings").classList.remove("d-none");
-    document.getElementById("button_password_recap").classList.remove("d-none");
+    //Action enabled
+    disableActionButtons(false);
 
     document.getElementById("password_ingame_status").innerHTML = "";
     document.getElementById("password_ingame_display").innerHTML = "...";
+
+    if (settings.use_timer_global == true) {
+        clearInterval(global.globalTimerInterval);
+    }
+
+    if (settings.use_timer_word == true) {
+        clearInterval(global.wordTimerInterval);
+    }
 }
 
-function quitPassword() {
-    displayPage("menu");
-    endPassword();
+function disableActionButtons(value) {
+    document.getElementById("button_password_invalidate").disabled = value;
+    document.getElementById("button_password_pass").disabled = value;
+    document.getElementById("button_password_validate").disabled = value;
 }
 
 function highlightWord(word) {
@@ -199,14 +199,19 @@ function updateIndicator(word, classesToAdd = [], classesToRemove = []) {
 }
 
 function passwordDisplayNextWord() {
-    const index = game.password.index;
+    const index = game.index;
 
-    if (index >= game.password.words.length) {
+    if (index >= game.words.length) {
         password_displayRecap();
         return;
     }
+    disableActionButtons(false);
 
-    const word = game.password.words[index];
+    if (settings.use_timer_word == true) {
+        initTimerWord()
+    }
+
+    const word = game.words[index];
     password_ingame_display.classList.remove("password-changing-word");
     setTimeout(() => password_ingame_display.classList.add("password-changing-word"), 1);
     setTimeout(() => {
@@ -217,49 +222,107 @@ function passwordDisplayNextWord() {
 }
 
 function passwordValidate() {
-    const index = game.password.index;
-    const word = game.password.words[index];
+    const index = game.index;
+    const word = game.words[index];
 
     updateIndicator(word, ["correct"], ["highlighted", "pass"]);
     word.status = "correct";
 
-    game.password.index++;
-    passwordDisplayNextWord();
+    game.index++;
 }
 
 function passwordInvalidate() {
-    const index = game.password.index;
-    const word = game.password.words[index];
+    const index = game.index;
+    const word = game.words[index];
 
     updateIndicator(word, ["disabled"], ["highlighted", "pass"]);
     word.status = "disabled";
 
-    game.password.index++;
-    passwordDisplayNextWord();
+    game.index++;
 }
 
 function passwordPass() {
-    const index = game.password.index;
-    const word = game.password.words[index];
+    const index = game.index;
+    const word = game.words[index];
 
     updateIndicator(word, ["pass"], ["highlighted"]);
     word.status = "pass";
 
     // 🔄 le "pass" revient en fin de liste
-    game.password.words.push(word);
-    game.password.index++;
-    passwordDisplayNextWord();
+    game.words.push(word);
+    game.index++;
+}
+
+function initTimerGlobal() {
+    clearInterval(global.globalTimerInterval);
+    timer_left = settings.timer_global;
+
+    const element = document.getElementById("password_timer");
+    element.innerHTML = timer_left;
+
+    global.globalTimerInterval = setInterval(() => {
+        timer_left--;
+        element.innerHTML = timer_left;
+
+        if (timer_left <= 0) {
+            clearInterval(global.globalTimerInterval);
+            password_displayRecap(); // fin de partie quand le chrono global est terminé
+        }
+        console.log(timer_left)
+    }, 1000);
+}
+
+function passwordDisplayNextWordDelayed() {
+    setTimeout(function() {
+        initTimerWord()
+        passwordDisplayNextWord();
+    }, global.automatic_timed_word_change_delay)
+}
+
+function initTimerWord() {
+    clearInterval(global.wordTimerInterval);
+    timer_left = settings.timer_word;
+
+    const element = document.getElementById("password_timer");
+    element.innerHTML = timer_left;
+
+    global.wordTimerInterval = setInterval(() => {
+        timer_left--;
+        element.innerHTML = timer_left;
+
+        if (timer_left <= 0) {
+            clearInterval(global.wordTimerInterval);
+            console.log("next word"); // fin de partie quand le chrono global est terminé
+            disableActionButtons(true);
+            passwordInvalidate()
+            passwordDisplayNextWordDelayed()
+        }
+        console.log(timer_left)
+    }, 1000);
 }
 
 function password_displayRecap() {
-    endPassword()
+    endPassword();
+
+    const data = game.words;
+
+    // supprimer les doublons par "id"
+    const reduced = Object.values(
+    data.reduce((acc, item) => {
+        acc[item.id] = item; // garde le dernier trouvé pour chaque id
+        return acc;
+    }, {})
+    );
+
+    // trier par id croissant
+    const sorted = reduced.sort((a, b) => a.id - b.id);
 
     password_recap_placeholder.innerHTML = "";
-    for (const word of game.password.words) {
+    for (const word of sorted) {
         password_recap_placeholder.innerHTML += 
             `<div class='centered recap_word password-shield ${word.status}'>
                 <div><span>${word.word}</span></div>
             </div>`;
     }
-    global.modal_recap.show()
+    global.modal_recap.show();
 }
